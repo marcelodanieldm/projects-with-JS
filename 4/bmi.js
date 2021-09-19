606d7b322347879aef66a816cbb6a8e5cb05cb5c
Marcelo Daniel <danielmarcelo83@gmail.com>
2021-09-19 16:30:33 -0300

    Add files via upload

---
+++ 4/bmi.js
@@ -0,0 +1,16 @@
+// bmi = kg/(h/100 * h/100)
+
+document.getElementById("submit").addEventListener("click", bmiCalculator);
+function bmiCalculator(){
+    var cm = parseInt(document.getElementById("cm").value);
+    var kg = parseFloat(document.getElementById("kg").value);
+
+    var bmi;
+    var newCm= parseFloat(cm/100);
+
+    bmi = kg / (newCm * newCm);
+    bmi = bmi.toFixed(1);
+    console.log(bmi);
+
+    document.getElementById("result").innerHTML = bmi;
+}
