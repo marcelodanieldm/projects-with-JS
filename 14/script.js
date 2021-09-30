abef8e65ad6cb1fc5a33dac3f8cad191250b1222
Marcelo Daniel <danielmarcelo83@gmail.com>
2021-09-30 09:49:57 -0300

    Add files via upload

---
+++ 14/script.js
@@ -0,0 +1,22 @@
+var d= Date();
+var weekday = new Array(7);
+weekday[0]= "Sunday";
+weekday[1]= "Monday";
+weekday[2]= "Tuesday";
+weekday[3]= "Wednesday";
+weekday[4]= "Thursday";
+weekday[5]= "Friday";
+weekday[6]= "Saturday";
+
+var date2 = new Date();
+var weekday2 = new Array(7);
+weekday[0]= "To to chillax! ";
+weekday[1]= "Monday morning blues! ";
+weekday[2]= "Taco time! ";
+weekday[3]= "Two more days to teh weekend. ";
+weekday[4]= "The weekend is almost here... ";
+weekday[5]= "Weekend is here!";
+weekday[6]= "Time to party! ";
+
+var n = weekday[d.getDay()];
+var n2 = weekday2[date2.getDay()];
