78b01062e23db9c798f66da72d4b0ac868f41145
Marcelo Daniel <danielmarcelo83@gmail.com>
2021-10-10 23:03:21 -0300

    Update eventos.js

--- references/eventos.js
+++ references/eventos.js
@@ -1,3 +1,15 @@
 
 //eventos del raton
 var boton = documentQuerySelector('boton');
+
+function cambiarColor(){
+  console.log('me cambiaste el color');
+  var bg=boton.style.background;
+  if(background=='green'){
+    boton.style.background='red';
+  }else{
+    boton.style.background='green';
+  }
+  return true;
+   
+}
