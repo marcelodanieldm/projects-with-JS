9f09694dc063e8e3712269db50adb0f353b522b3
Marcelo Daniel <danielmarcelo83@gmail.com>
2022-01-13 18:16:12 -0300

    Update challenge4.js

--- references/ejerciciostartcodingai/challenge4.js
+++ references/ejerciciostartcodingai/challenge4.js
@@ -3,3 +3,7 @@
 const miArray = ["juan", "pedro", "marcela"];
 const firstElement = miArray[0];
 console.log(firstElement);
+
+//acceder al ultimo
+console.log(miArray[2]);
+console.log(miArray[miArray.length -1]);
