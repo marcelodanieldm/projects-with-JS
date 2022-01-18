a28205b3c0c073c1fc77f3477b95849a103d2fda
Marcelo Daniel <danielmarcelo83@gmail.com>
2022-01-18 20:19:36 -0300

    Update challenge4.js

--- references/ejerciciostartcodingai/challenge4.js
+++ references/ejerciciostartcodingai/challenge4.js
@@ -122,6 +122,8 @@ console.log(invitados.length);
 //2 Informar por consola el nombre del ultimo invitado
 console.log([invitados.length -1 ]);
 //3 Agregar a Pedro a la lista
+invitados.push("Pedro");
+console.log(invitados);
 //4 Un rato antes del comienzo de la fiesta, Evelina avisa que no puede ir. Generar una lista con los invitados que confirmaron su asistencia.
 //5 Por cada invitado, asignarle un numero de orden en la lista.
                             
