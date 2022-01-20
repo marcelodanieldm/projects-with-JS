9a6f1d682399c0a4a51d44c1fa51c8b7a8d9f303
Marcelo Daniel <danielmarcelo83@gmail.com>
2022-01-20 16:16:51 -0300

    examples of objects in js

---
+++ references/ejerciciostartcodingai/challenge5.js
@@ -0,0 +1,8 @@
+//objetos es una coleccion de valores desordenada. Asi se almacena la data en los objetos: {key:value}
+// declaracion de objetos en js 
+const literalObject = {firstName:"Juan"};
+console.log(literalObject);
+
+//otra forma
+const normalObject = new Object();
+console.log(normalObject);
