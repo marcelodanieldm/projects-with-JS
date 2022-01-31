55359709226647bf882351006316cb4d44d8c476
Marcelo Daniel <danielmarcelo83@gmail.com>
2022-01-30 21:46:21 -0300

    consignas

---
+++ references/ejerciciostartcodingai/challenge5c.js
@@ -0,0 +1,8 @@
+//Andrea y Martin van de picnic. Andrea propone llevar una bolsa con dos cervezas, una de dos sandwiches, y una de papas fritas. Por su parte, Martin lleva una porcion de torta,
+//una gaseosa y un paquete de galletitas dulces.
+//Crear un objeto que represente a cada una de las bolsas. Donde cada producto tenga la cantidad indicada. 
+//a) Mostrar por consola los productos que llevo cada uno.
+//b) En el camino al picnic, la bolsa de De Andrea se rompio. Por lo que tuvieron que poner todo en la bolsa de Martin.
+//Realizar el cambio correspondiente para que los cambios esten todos en la bolsa de Martin.
+//Calcular el total de productos que llevaron al picnic.
+
