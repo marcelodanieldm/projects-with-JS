5047b4d6604ce455accadac60937a02d96d6edda
Marcelo Daniel <danielmarcelo83@gmail.com>
2022-02-16 13:39:54 -0300

    Update challenge6.js

--- references/ejerciciostartcodingai/challenge6.js
+++ references/ejerciciostartcodingai/challenge6.js
@@ -1,5 +1,5 @@
- //asincronismo
+  //asincronismo
  //setTimeout
-setTimeOut(() => console.log("Mario"), 0);
-console.log("Juan");
-console.log("Pedro");
+ setTimeOut(() => console.log("Mario"), 0);
+ console.log("Juan");
+ console.log("Pedro");
